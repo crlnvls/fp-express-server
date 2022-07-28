739e6a960f0fa7fa0e1c52aef44b77079ab8a9f0
Matt Lilley <m.k.lilley@gmail.com>
2022-07-28 18:44:22 +0100

    Updates start-docker.sh to include an additional volume mount

--- start-container.sh
+++ start-container.sh
@@ -4,6 +4,7 @@ docker run -it \
 --name express-api \
 -w /code \
 --mount type=bind,source="$(pwd)",target=/code \
+--mount type=volume,source=express-compose_visits,target=/data \
 node:latest \
 bash -c "npm install && npm run dev"
 # Instead of -e PORT=3000 we can do --env-file .env
